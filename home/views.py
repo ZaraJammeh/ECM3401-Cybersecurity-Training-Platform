2b6e07fe8e5272eef7b1e186963532b32a4e2c23
ZaraJammeh <126472155+ZaraJammeh@users.noreply.github.com>
2025-03-09 18:04:10 +0000

    Create landing page and dashboard - Add views for landing page and dashboard - Create html template for landing page with placeholder text - Create html template for dashboard page with placeholder text - Link html templates to views.py - Update home/urls.py

--- home/views.py
+++ home/views.py
@@ -1,4 +1,8 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("It's the home page!")
+    return render(request, "home/index.html")
+
+def dashboard(request):
+    return render(request, "home/dashboard.html")
