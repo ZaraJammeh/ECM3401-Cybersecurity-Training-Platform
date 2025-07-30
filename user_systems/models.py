6a874074d8910e0ae7f753da60bfd856b7099f05
ZaraJammeh <126472155+ZaraJammeh@users.noreply.github.com>
2025-07-30 11:35:01 +0100

    Create DB models for storing user knowledge data and activity courses (Additionally edit dashboard page with variable header message)

--- user_systems/models.py
+++ user_systems/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils import timezone
 
+from core.models import Subtopic, KnowledgePoint, Activity
+
 # Code for custom users heavily based on:
 # https://testdriven.io/blog/django-custom-user-model/
 
@@ -43,3 +45,49 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+class Course(models.Model):
+    estim_duration = models.DurationField()
+    length = models.IntegerField()
+    next_activity = models.IntegerField(default=1)
+
+class CourseData(models.Model):
+    parent_course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    activity_id = models.ForeignKey(Activity, on_delete=models.CASCADE)
+    index = models.IntegerField()
+    
+class CurrentCourse(models.Model):
+    # links user and course (*:1) externally from user model
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+class SubtopicCoverage(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    subtopic = models.ForeignKey(Subtopic, on_delete=models.CASCADE)
+    last_covered_on = models.DateField()
+
+class Ratings(models.IntegerChoices):
+        # enum for rating user knowledge of each kp
+        UNSEEN = 0
+        SEEN = 1
+        PRACTISED = 2
+        APPLIED = 3
+        MASTERED = 4
+
+class UserKnowledgeData(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    kp = models.ForeignKey(KnowledgePoint, on_delete=models.CASCADE)
+    rating = models.IntegerField(choices=Ratings)
+    updated_on = models.DateField()
+
+    def set_rating(self, new_rating):
+        self.rating = new_rating
+    
+class UserKnowledgeUpdate(models.Model):
+    # used to store pending knowledge updates
+    # submitted to UserKnowledgeData via POST when an Activity is completed
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    kp = models.ForeignKey(KnowledgePoint, on_delete=models.CASCADE)
+    rating = models.IntegerField(choices=Ratings)
+    # store ID of posting Activity to prevent cheated scores through memorising + reattempting
+    anticheat_id = models.ForeignKey(Activity, on_delete=models.CASCADE)
